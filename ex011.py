169d8fe269b08a08edd8206c969211dcb7c286eb
Andreson94 <andreson.dc.silva@gmail.com>
2021-01-19 19:25:49 -0300

    calculo basico

---
+++ ex011.py
@@ -0,0 +1,7 @@
+
+larg = float(input('largura:'))
+alt = float(input('altura: '))
+area = larg * alt
+print('sua parede tem é de {}x{} e sua area é de {}m²'.format(larg,alt,area))
+tinta = area/2
+print('voce precisa de {}L de tinta para pinta essa parede'.format(tinta))
