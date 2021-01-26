1882450d447ef44ce924f7540f09a15df3525a4c
Andreson94 <andreson.dc.silva@gmail.com>
2021-01-26 16:49:00 -0300

    len split upper

---
+++ exe022.py
@@ -0,0 +1,12 @@
+#– O nome com todas as letras maiúsculas e minúsculas.
+#– Quantas letras ao todo (sem considerar espaços).
+#– Quantas letras tem o primeiro nome.
+
+nome = input('Nome Completo: ')
+print(nome.upper())
+print(len(nome))
+n = nome.split()
+print(len(n [0]))
+
+
+
