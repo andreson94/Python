fa828ccc3a5a5779dc0878cb9516377ab0bacbaf
Andreson94 <andreson.dc.silva@gmail.com>
2021-01-26 16:58:47 -0300

    len split upper

--- exe022.py
+++ exe022.py
@@ -2,9 +2,9 @@
 #– Quantas letras ao todo (sem considerar espaços).
 #– Quantas letras tem o primeiro nome.
 
-nome = input('Nome Completo: ')
+nome = str( input('Nome Completo: '))
 print(nome.upper())
-print(len(nome))
+print(len(nome) - nome.count(' '))
 n = nome.split()
 print(len(n [0]))
 
