e53581e7f3cbbbb5fbde93f02432d5614dbacfd8
Andreson94 <andreson.dc.silva@gmail.com>
2021-02-03 17:22:10 -0300

    condicionamento composto if else radar

---
+++ exe029.py
@@ -0,0 +1,10 @@
+# Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que
+# ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.
+
+
+v = int(input('Qual foi sua velocidade?'))
+if v > 80 :
+    m = (v - 80) * 7
+    print('voce foi multado em R$ {}'.format(m))
+else:
+    print('voce estava dentro do limite de velocidade')
