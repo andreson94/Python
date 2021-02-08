d120e6d2f4fbef0f7d47f04a68edabb711748292
Andreson94 <andreson.dc.silva@gmail.com>
2021-02-07 21:01:21 -0300

    #Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, # cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 parta viagens mais long

---
+++ exe031.py
@@ -0,0 +1,12 @@
+#Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem,
+# cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 parta viagens mais long
+
+
+n = float(input('Qual a distancia da sua viajem em km: '))
+
+if n > 200:
+    km = n * 0.45
+    print('Sua passagem sairá R${:.2f}'.format(km))
+else:
+    km = n * 0.50
+    print('sua passagem custará R${:.2f}'.format(km))
