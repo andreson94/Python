dcb7e84875bc65e4e5aa000ee1e90a2ac7e21873
Andreson94 <andreson.dc.silva@gmail.com>
2021-02-13 21:59:04 -0300

    cores

---
+++ cores.py
@@ -0,0 +1,19 @@
+cores = {'limpa':'\033[m',
+         'braco':'\033[30m',
+         'vermelho':'\033[31]'}
+
+
+#são separadas por cores texto 30 a 37, estilo (0,1,4,7) , fundo 40 a 47
+# 0 = padrao
+# 1 = vermelho
+# 2 = verde
+# 3 = amarelo
+# 4 = azul
+# 5 = lilas
+# 6 = azul bebe
+# 7 = cinza
+
+# 0 = padrao
+# 1 = negrito
+# 4 = sublinhado
+# 7 = inverte padrao de cores
