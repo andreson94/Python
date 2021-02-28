c27ab4db7cb9455ff35bed03f5ee5a3e9420177a
Andreson94 <andreson.dc.silva@gmail.com>
2021-02-27 21:57:33 -0300

    triangulo v2

---
+++ exe043.py
@@ -0,0 +1,20 @@
+print('\033[1;31m  *** IMC ***\033[m')
+#recebendo informações do usuario
+peso = float(input('\033[34mQual o seu peso?\033[m '))
+alt = float(input('\033[34mQual a sua altura? \033[m')) / 100
+#fazendo calculo do imc
+imc = peso / (alt * alt)
+#condicionando os resultados
+if imc < 18.5 :
+    print('Seu IMC é {:.2f}, voce esta abaixo do peso'.format(imc))
+elif imc <= 25:
+    print('IMC {:.2f} Peso ideal'.format(imc))
+elif imc <= 30:
+    print('IMC {:.2f} sobrepeso'.format(imc))
+elif imc <= 40:
+    print('IMC {:.2f} obesidade'.format(imc))
+else:
+    print('obesidade morbida')
+
+
+
