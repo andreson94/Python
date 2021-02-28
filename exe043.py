5d49495d795cc135f38ada0671d8c59b5c8a5c17
Andreson94 <andreson.dc.silva@gmail.com>
2021-02-27 22:04:21 -0300

    IMC

--- exe043.py
+++ exe043.py
@@ -3,16 +3,16 @@ print('\033[1;31m  *** IMC ***\033[m')
 peso = float(input('\033[34mQual o seu peso?\033[m '))
 alt = float(input('\033[34mQual a sua altura? \033[m')) / 100
 #fazendo calculo do imc
-imc = peso / (alt * alt)
+imc = peso / (alt ** 2)
 #condicionando os resultados
 if imc < 18.5 :
-    print('Seu IMC é {:.2f}, voce esta abaixo do peso'.format(imc))
+    print('Seu IMC é {:.1f}, voce esta abaixo do peso'.format(imc))
 elif imc <= 25:
-    print('IMC {:.2f} Peso ideal'.format(imc))
+    print('IMC {:.1f} Peso ideal'.format(imc))
 elif imc <= 30:
-    print('IMC {:.2f} sobrepeso'.format(imc))
+    print('IMC {:.1f} sobrepeso'.format(imc))
 elif imc <= 40:
-    print('IMC {:.2f} obesidade'.format(imc))
+    print('IMC {:.1f} obesidade'.format(imc))
 else:
     print('obesidade morbida')
 
