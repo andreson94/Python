e30f3b58c27c460cd785dbda7b36ef9a99a5cd79
Andreson94 <andreson.dc.silva@gmail.com>
2021-03-04 18:36:52 -0300

    laço de repetição

--- exe050.py
+++ exe050.py
@@ -1,7 +1,7 @@
 print('10 termos de uma PA')
 t = int(input('Primeiro termo: '))
 r = int(input('Razão: '))
-d = t + (10 - 1) * r
-for c in range(t, d + r, r):
+d = t + (10 - 1) * r #formula para saber o prdecimo termo
+for c in range(t, d + r, r): #laço que vai ate o decimo termo
     print(c, end= '->')
 print('acabou')
