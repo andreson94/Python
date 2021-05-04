e44996a7b8d6f70b9de1bd4823809cc21de97e45
Andreson94 <andreson.dc.silva@gmail.com>
2021-05-04 13:55:00 -0300

    sorteando numeros na tupla simplificado

---
+++ exe073_simplificado.py
@@ -0,0 +1,3 @@
+from random import randint
+n = (randint(1,10),randint(1,10),randint(1,10),randint(1,10),randint(1,10))
+print(f'Os numeros sorteados foram {n} \nO maior foi {max(n)} \nO menor foi {min(n)}')
